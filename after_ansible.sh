700ae7fbfd0cfe454fd3ef3ba243a3d6e3ab2771
Serhii Shvets <wert2all@gmail.com>
2026-01-31 13:04:50 +0200

    fix: fix port ansible

--- after_ansible.sh
+++ after_ansible.sh
@@ -5,7 +5,14 @@ WORK_DIR="$HOME/work"
 systemctl enable --user ssh-agent
 systemctl start --user ssh-agent
 
-cd $WORK_DIR
-git clone https://aur.archlinux.org/yay.git
-cd yay
-makepkg -si
+# cd $WORK_DIR
+# git clone https://aur.archlinux.org/yay.git
+# cd yay
+# makepkg -si
+
+yay -S --noconfirm \
+  zen-browser-bin
+
+pnpm install -g opencode-ai @fission-ai/openspec@latest
+
+curl -fsSL https://plannotator.ai/install.sh | bash
