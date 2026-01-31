80d2b5fe2440f5fd5a086a4287ef65b03cecd98b
Serhii Shvets <wert2all@gmail.com>
2026-01-31 12:55:12 +0200

    feat: install yay

--- after_ansible.sh
+++ after_ansible.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
+WORK_DIR="$HOME/work"
+
 systemctl enable --user ssh-agent
 systemctl start --user ssh-agent
+
+cd $WORK_DIR
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si
